d7257c3c95bbc5a6b031ce97f79a08b58fa03a85
brittanyrw <breneewalker@gmail.com>
2019-06-25 23:41:23 -0400

    change hello world component to hero component

---
+++ src/components/Hero.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="hero">
+    <h1>Millennials Slay</h1>
+    <p>A directory of industries, traditions and products that millenials have <span>killed</span> slayed over the years.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hero'
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+.hero span {
+  text-decoration: line-through;
+}
+</style>
