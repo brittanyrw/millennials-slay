510b4a2a5987baa7e42d98621e6761fb1d522e24
brittanyrw <brittany.walker@socialdriver.com>
2019-07-02 15:37:45 -0400

    add new placeholder images and placeholder data

--- src/data/data.js
+++ src/data/data.js
@@ -3,6 +3,25 @@ export const dataList = [
 		name: 'Napkins',
 		summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
 		year: 2019,
+		image: 'napkin.jpeg',
+		why: 'In porta odio ullamcorper risus tempor maximus. Donec semper lacus vitae odio laoreet euismod.',
+		sources: [
+			{
+				name: 'Washington Post',
+				year: 2019,
+				link: 'https://www.google.com'	
+			},
+			{
+				name: 'New York Times',
+				year: 2018,
+				link: 'https://www.google.com'	
+			}
+		]
+	},
+	{
+		name: 'Divorce',
+		summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+		year: 2019,
 		image: 'divorce.jpeg',
 		why: 'In porta odio ullamcorper risus tempor maximus. Donec semper lacus vitae odio laoreet euismod.',
 		sources: [
@@ -22,7 +41,7 @@ export const dataList = [
 		name: 'Beer',
 		summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
 		year: 2011,
-		image: 'divorce.jpeg',
+		image: 'beer.jpeg',
 		why: 'In porta odio ullamcorper risus tempor maximus. Donec semper lacus vitae odio laoreet euismod.',
 		sources: [
 			{
@@ -41,7 +60,7 @@ export const dataList = [
 		name: 'Cereal',
 		summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
 		year: 2011,
-		image: 'divorce.jpeg',
+		image: 'cereal.jpeg',
 		why: 'In porta odio ullamcorper risus tempor maximus. Donec semper lacus vitae odio laoreet euismod.',
 		sources: [
 			{
