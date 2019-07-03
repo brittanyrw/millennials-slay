aceaef3295f31e00b4fb4c225d42058f154b413e
brittanyrw <brittany.walker@socialdriver.com>
2019-07-03 17:33:54 -0400

    add placeholders for filters

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -2,7 +2,13 @@
   <div class="hero">
     <h1>Millennials Slay</h1>
     <p>A directory of industries, traditions and products that millenials have <span>killed</span> slayed over the years.</p>
-    <div class="filters">[FILTERS HERE]</div>
+    <div class="filters">
+      <p class="filter-label">Filters: </p>
+      <p class="filter">Products</p>
+      <p class="filter">Places</p>
+      <p class="filter">Traditions</p>
+      <p class="filter">Activities</p>
+    </div>
   </div>
 </template>
 
@@ -13,5 +19,14 @@ export default {
 </script>
 
 <style scoped>
+.filters p {
+  display: inline-block;
+  margin-right: 10px;
+}
 
+.filters .filter {
+  border: 1.5px solid #ccc;
+  padding: 5px 10px;
+  border-radius: 3px;
+}
 </style>
